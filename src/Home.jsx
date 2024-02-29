a0d8378076fb551e3b23a6dbe7b146f23325db45
Portal <juanportalg@gmail.com>
2024-02-28 21:10:24 -0800

    home finished

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 export const Home = () => {
   return (
-    <section>
+    <section className='home'>
         <h2>Home</h2>
+        <div className="container">
+            <img src="./assets/home.jpg" alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae commodi in dicta facilis, ab eius voluptatibus repellat perferendis sit eligendi alias ipsum maxime, quae veritatis debitis nulla, voluptates officia consectetur illum? Provident, facilis odit! Ea natus placeat maiores culpa fuga aspernatur saepe id accusamus? Ut fuga ab voluptatum? Dolorum unde ratione necessitatibus illo, earum ea possimus, commodi est porro laboriosam aspernatur reiciendis voluptates dolores rerum iure et nostrum! Iure hic in at, accusamus eum ab accusantium adipisci dolorem minima ex. Accusamus nihil neque iure sit, placeat fugiat ducimus nesciunt amet velit perspiciatis necessitatibus ipsa consequatur, expedita labore suscipit reiciendis quod.</p>
+        </div>
     </section>
   )
 }
