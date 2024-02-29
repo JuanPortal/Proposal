9fbd43a4b6c72eb1587c9c02b6f88b21481337dd
Portal <juanportalg@gmail.com>
2024-02-28 18:15:07 -0800

    routes implemented

---
+++ src/Gallery.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Gallery = () => {
+  return (
+    <h2>Gallery</h2>
+  )
+}
--- src/Template.jsx
+++ src/Template.jsx
@@ -1,8 +1,21 @@
+import { Route, Routes } from "react-router-dom"
+import { Home } from "./Home"
+import { Services } from "./Services"
+import { Gallery } from "./Gallery"
+import { Contact } from "./Contact"
+
 function Template() {
 
   return (
     <>
       <h1>Template</h1>
+
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/services" element={<Services />}/>
+        <Route path="/gallery" element={<Gallery />}/>
+        <Route path="/contact" element={<Contact />}/>
+      </Routes>
     </>
   )
 }
