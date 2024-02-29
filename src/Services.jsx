4c19aa1c3f85a84472f8ae505b53faf73c0087cc
Portal <juanportalg@gmail.com>
2024-02-28 23:11:31 -0800

    services finished

--- src/Services.jsx
+++ src/Services.jsx
@@ -2,8 +2,32 @@ import React from 'react'
 
 export const Services = () => {
   return (
-    <section>
+    <section className='services'>
         <h2>Services</h2>
+
+        <div className="service">
+            <h3>Service 1</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex iusto iure officia sed consectetur nulla praesentium reprehenderit eaque pariatur rerum?</p>
+            <img src="./assets/service 1.jpg" title="service 1" alt="service 1" />
+        </div>
+
+        <div className="service">
+            <h3>Service 2</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex iusto iure officia sed consectetur nulla praesentium reprehenderit eaque pariatur rerum?</p>
+            <img src="./assets/service 2.jpg" title="service 2" alt="service 2" />
+        </div>
+
+        <div className="service">
+            <h3>Service 3</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex iusto iure officia sed consectetur nulla praesentium reprehenderit eaque pariatur rerum?</p>
+            <img src="./assets/service 3.jpg" title="service 3" alt="service 3" />
+        </div>
+        
+        <div className="service">
+            <h3>Service 4</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex iusto iure officia sed consectetur nulla praesentium reprehenderit eaque pariatur rerum?</p>
+            <img src="./assets/service 4.jpg" title="service 4" alt="service 4" />
+        </div>
     </section>
   )
 }
