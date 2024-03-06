6eb1dc0138e381fc998b1a427e3909195cab5df7
Portal <juanportalg@gmail.com>
2024-03-05 23:53:42 -0800

    responsive implemented

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import Calendar from './Calendar'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram, faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faLocationDot, faPhone } from '@fortawesome/free-solid-svg-icons'
 
 export const Contact = () => {
   return (
     <section className='contact'>
         <h2>Contact</h2>
 
-        <Calendar />
-
-        <a href="tel:+8181234567">(818) 123 - 4567</a>
-        <address>11600 Sherman Way, North Hollywood, CA 91605</address>
+        <div className="socials">
+          <a href="https://instagram.com" target='_blank'><FontAwesomeIcon icon={faInstagram} /></a>
+          <a href="https://facebook.com" target='_blank'><FontAwesomeIcon icon={faFacebook} /></a>
+          <a href="https://x.com" target='_blank'><FontAwesomeIcon icon={faTwitter} /></a>
+        </div>
+        <a href="tel:+8181234567"><FontAwesomeIcon icon={faPhone} />(818) 123 - 4567</a>
+        <a href="https://maps.app.goo.gl/qf2awk4EoxHT9Bhi8" target='_blank'><FontAwesomeIcon icon={faLocationDot} /><address>11600 Sherman Way, North Hollywood, CA 91605</address></a>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3299.901419043156!2d-118.38598987365975!3d34.19999510991311!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2942dbaa3387d%3A0xcda5ec412bb0b52c!2s11600%20Sherman%20Way%2C%20North%20Hollywood%2C%20CA%2091605!5e0!3m2!1sen!2sus!4v1709194340790!5m2!1sen!2sus" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+        <Calendar />
     </section>
   )
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,20 +1,47 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
 
 export const Header = () => {
+  const location = useLocation();
+
+  const [hamburgerVisibility, setHamburgerVisibility] = useState('invisible')
+  const [hamburgerIcon, setHamburgerIcon] = useState(faBars)
+
+  const handleHamburgerIcon = () => {
+    if (hamburgerIcon === faBars) {
+      setHamburgerIcon(faXmark)
+      setHamburgerVisibility('visible')
+    } else {
+      setHamburgerIcon(faBars)
+      setHamburgerVisibility('invisible')
+    }
+  }
+
+  const handleLinkClick = () => {
+    setHamburgerIcon(faBars)
+    setHamburgerVisibility('invisible')
+  }
+
   return (
     <header>
-        <h1>
-          <Link to='/'>
-              <img src="https://firebasestorage.googleapis.com/v0/b/template-64987.appspot.com/o/logo.webp?alt=media&token=eb051ac6-fd50-4487-abaa-186e48b4b218" title="logo" alt="logo" />
-              <p>Template</p>
-          </Link>
-        </h1>
-      <nav>
-        <Link className='link' to='/'>Home</Link>
-        <Link className='link' to='/services'>Services</Link>
-        <Link className='link' to='/contact'>Contact</Link>
+      <h1>
+        <Link to='/'>
+          <img src="https://firebasestorage.googleapis.com/v0/b/template-64987.appspot.com/o/logo.webp?alt=media&token=eb051ac6-fd50-4487-abaa-186e48b4b218" title="logo" alt="logo" />
+          <p>Template</p>
+        </Link>
+      </h1>
+
+      <nav className={`${hamburgerVisibility}`}>
+        <Link onClick={handleLinkClick} className={`link ${location.pathname === '/' ? 'active-link' : ''}`} to='/'>Home</Link>
+        <Link onClick={handleLinkClick} className={`link ${location.pathname === '/services' ? 'active-link' : ''}`} to='/services'>Services</Link>
+        <Link onClick={handleLinkClick} className={`link ${location.pathname === '/contact' ? 'active-link' : ''}`} to='/contact'>Contact</Link>
       </nav>
+
+      <div className="hamburger-menu" onClick={handleHamburgerIcon}>
+        <FontAwesomeIcon icon={hamburgerIcon} />
+      </div>
     </header>
   )
 }
